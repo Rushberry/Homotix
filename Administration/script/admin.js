569aefa7f818bf9722a7753094ed6654ad391f67
Md. Rafsan Afnan Rushan <111191121+Rushberry@users.noreply.github.com>
2024-02-10 03:56:38 +0000

    Alhamdulillah

---
+++ Administration/script/admin.js
@@ -0,0 +1,3 @@
+if (!localStorage.getItem('authenticated')) {
+    window.location.href = 'Administration/p-admin.html';
+}
---
+++ Administration/script/p-admin.js
@@ -0,0 +1,24 @@
+function authenticate() {
+    var password = document.getElementById('password').value;
+    if (password === 'Ana Rushan Ibn Anwar') {
+        localStorage.setItem('authenticated', 'true');
+        window.location.href = '../administration.html';
+    } else {
+        alert('Incorrect password. Please try again.');
+    }
+}
+
+var timer; 
+function showPassword() {
+    var passwordInput = document.getElementById('password');
+    passwordInput.type = 'text';
+    }
+function hidePassword() {
+    var passwordInput = document.getElementById('password');
+    passwordInput.type = 'password';
+    }
+function onKeyPress() {
+    clearTimeout(timer);  
+    showPassword();
+    timer = setTimeout(hidePassword, 1000);
+    }
