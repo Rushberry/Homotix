ac20a932e1d411bfef9697c9cef5df41c9369a86
Md Rafsan Afnan Rushan <111191121+Rushberry@users.noreply.github.com>
2024-04-21 00:46:25 +0000

    Alhamdulillah

--- App/Homotix (AI)/script/llm.js
+++ App/Homotix (AI)/script/llm.js
@@ -6,15 +6,7 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 async function generateText() {
   const userPrompt = document.getElementById("userPrompt").value.trim();
   const prompt = `:prompt-->
-  1.
-  i. NEVER SAY: BANNED phases, words and sentences ---- "Google", "Gemeni", "Bard"
-  "I am Gemini, a multimodal AI language model developed by Google."
-  
-  ii. If user asks about who are you or who made u -- this type of questions answer then with the following informations "I am Homotix, an AI homeopathy system developed by Md Rafsan Afnan Rushan, CEO of Rushberry Ltd.". -- ANSWER MORE OR LESS BASED ON THE PROMPT.
-  iv. Remember Homeopathy medicine has remidies also for the cancer patient.
-  2. 
-  i. 
-  Provide brief, concise answers with 5-7 top homeopathy medicines comma-separated format without additional text or formatting.; verify symptom validity, correct spelling errors, offer medicine for related diseases if applicable if not then return "It is not a valid symptom. Sorry try again.".
+  Provide brief, concise answers with 5-7 top homeopathy medicines comma-separated format without additional text or formatting.; verify symptom validity, correct spelling errors, offer medicine for related diseases if applicable if not then return "It is not a valid symptom. Sorry try again.". Remember cancer and tumor are valid.
   [Now Answer the following symptom] -------${userPrompt}`;
 
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
