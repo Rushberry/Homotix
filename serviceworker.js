ac8d06a04ff1fbe7cc503ad26873fdd3551ccbdc
Md Rafsan Afnan Rushan <111191121+Rushberry@users.noreply.github.com>
2024-04-26 12:00:25 +0000

    Alhamdulillah

--- serviceworker.js
+++ serviceworker.js
@@ -1,17 +1,18 @@
-const CACHE_NAME = 'homotix-cache';
+const CACHE_NAME = 'homotix-cache-v1';
 const urlsToCache = [
   '/offline.html',
   '/Mobile/offline.css',
   '/Mobile/Homotix.gif',
   '/Mobile/H512.png',
   '/Mobile/H192.png',
-  'manifest.json'
+  '/manifest.json' // Ensure this is the correct path to your manifest.json
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .catch(error => console.error('Error caching files:', error))
   );
 });
 
@@ -22,18 +23,21 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        return fetch(event.request);
+        return fetch(event.request)
+          .catch(error => {
+            console.error('Error fetching:', error);
+            return caches.match('/offline.html'); // Return offline page if fetch fails
+          });
       })
   );
 });
 
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
