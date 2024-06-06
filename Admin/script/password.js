99fb6932372a9285c93828ff6846569412739507
Md Rafsan Afnan Rushan <111191121+Rushberry@users.noreply.github.com>
2024-06-06 14:07:06 +0000

    Alhamdulillah

--- Admin/script/password.js
+++ Admin/script/password.js
@@ -3,6 +3,9 @@ function authenticate() {
     if (password === 'Alhamdulillah') {
         localStorage.setItem('authenticated', 'true');
         window.location.href = '../admin';
+    } else if (password === 'HXATAMIM') {
+        localStorage.setItem('authenticated', 'true');
+        window.location.href = '../agend';
     } else {
         alert('Incorrect password. Please try again.');
     }
@@ -12,13 +15,13 @@ var timer;
 function showPassword() {
     var passwordInput = document.getElementById('password');
     passwordInput.type = 'text';
-    }
+}
 function hidePassword() {
     var passwordInput = document.getElementById('password');
     passwordInput.type = 'password';
-    }
+}
 function onKeyPress() {
     clearTimeout(timer);  
     showPassword();
     timer = setTimeout(hidePassword, 1000);
-    }
+}
